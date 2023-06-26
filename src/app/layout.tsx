3cb6747a6a90d1259a42d88d56d1628e8fcaeaf2
dongree <dongwoo0307@naver.com>
2023-06-26 13:43:19 +0900

    Fix: 기본 setting 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import Header from '@/components/Header';
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const sans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={sans.className}>
+      <body>
         <Header />
         {children}
       </body>
