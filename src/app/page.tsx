040777bce376228f8ca3b0b35bdd493e54e9946f
dongree <dongwoo0307@naver.com>
2023-06-26 14:25:29 +0900

    Fix: 전체적인 레이아웃, Header 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import CardBoard from '@/components/CardBoard';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <section className="flex flex-col items-center">
       <Profile />
       <section className="w-[calc(100%_-_5rem)]">
         <p className="text-lg font-bold">Featured Post</p>
         <CardBoard />
       </section>
-    </main>
+    </section>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,15 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="flex justify-between bg-gray-100 px-10 py-5">
-      <Link href="/" className=" font-bold text-xl">
-        dongrees blog
+    <header className="flex justify-between items-center bg-gray-100 px-10 py-5">
+      <Link href="/">
+        <h1 className=" font-bold text-xl">{"dongree's blog"}</h1>
       </Link>
-      <nav>
-        <Link href="/" className="px-2">
-          home
-        </Link>
-        <Link href="/about" className="px-2">
-          about
-        </Link>
-        <Link href="/posts" className="px-2">
-          posts
-        </Link>
-        <Link href="/contact" className="px-2">
-          contact
-        </Link>
+      <nav className="flex gap-4">
+        <Link href="/">home</Link>
+        <Link href="/about">about</Link>
+        <Link href="/posts">posts</Link>
+        <Link href="/contact">contact</Link>
       </nav>
     </header>
   );
