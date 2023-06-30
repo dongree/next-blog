51efddf0431e2c1a7098ee7503e6741973b04c29
dongree <dongwoo0307@naver.com>
2023-06-30 23:27:46 +0900

    Style: Markdown Viewer syntax highlight 설정, 스타일 수정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import MarkdownViewer from '@/components/MarkdownViewer';
 import { getPostData } from '@/service/posts';
+import Image from 'next/image';
+import { AiFillCalendar } from 'react-icons/ai';
 
 type Props = {
   params: {
@@ -8,12 +10,27 @@ type Props = {
 };
 
 export default async function Postpage({ params: { slug } }: Props) {
-  const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
 
   return (
-    <>
-      <h1>{post.title}</h1>
-      <MarkdownViewer content={post.content} />
-    </>
+    <article className="rounded-2xl overflow-hidden bg-gray-50 m-4">
+      <Image
+        src={`/img/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+        className="w-full max-h-[500px]"
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-sky-600">
+          <AiFillCalendar />
+          <time className="font-semibold ml-2">{date.toString()}</time>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-lg font-semibold">{description}</p>
+        <div className=" h-1 w-40 bg-sky-600 mb-10 mt-3"></div>
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
